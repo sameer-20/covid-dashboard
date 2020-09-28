cb30340e3daadfbb42d82e7970b0ea8c34aa83f1
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-28 03:49:21 +0000

    Added cases to return state based on covid data

--- client/src/reducers/dataReducer.js
+++ client/src/reducers/dataReducer.js
@@ -1,23 +1,49 @@
-export const SET_MAP_DATA = "SET_MAP_DATA";
-export const SET_USER_FAVOURITES = "SET_USER_FAVOURITES";
+export const SET_USERS = "SET_USERS";
+export const SET_HISTORICAL_GLOBAL = "SET_HISTORICAL_GLOBAL";
+export const SET_YESTERDAY_CONTINENTS = "SET_YESTERDAY_CONTINENTS";
+export const SET_YESTERDAY_GLOBAL = "SET_YESTERDAY_GLOBAL";
 
-const mapReducer = (state, action) => {
+const dataReducer = (state, action) => {
+  // const actions = {
+  //   SET_USERS: {
+  //     ...state,
+  //     users: action.users,
+  //     loading: false,
+  //   },
+  // };
+
   switch (action.type) {
-    case SET_MAP_DATA: {
+    case SET_USERS: {
       return {
         ...state,
-        mapData: action.mapData,
+        users: action.users,
+        loading: false,
+      };
+    }
+    case SET_HISTORICAL_GLOBAL: {
+      return {
+        ...state,
+        globalHistorical: action.globalHistorical,
         loading: false,
       };
     }
 
-    case SET_USER_FAVOURITES: {
+    case SET_YESTERDAY_CONTINENTS: {
       return {
         ...state,
-        userFavourites: action.userFavourites,
+        yesterdayContinents: action.yesterdayContinents,
         loading: false,
       };
     }
+
+    case SET_YESTERDAY_GLOBAL: {
+      return {
+        ...state,
+        yesterdayGlobal: action.yesterdayGlobal,
+        loading: false,
+      };
+    }
+
 
     default:
       throw new Error(
@@ -28,4 +54,4 @@ const mapReducer = (state, action) => {
   // return actions[action.type] || state;
 };
 
-export default mapReducer;
+export default dataReducer;
