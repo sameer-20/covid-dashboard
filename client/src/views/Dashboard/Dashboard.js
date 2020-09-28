20f4761690e6756c7c6e1d1487cadef662e291ab
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-28 03:51:43 +0000

    Updated dashboard view to display required charts

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -29,235 +29,167 @@ import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
+import CasesChart from "components/CasesChart.js";
+import ChartPie from "components/ChartPie.js";
+import CardDashboard from "components/CardDashboard.js";
+
 import { bugs, website, server } from "variables/general.js";
 
-import {
-  dailySalesChart,
-  emailsSubscriptionChart,
-  completedTasksChart
-} from "variables/charts.js";
-
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+import "../../App.scss";
+
+import useApplicationData from "../../hooks/useApplicationData";
 
 const useStyles = makeStyles(styles);
 
 export default function Dashboard() {
+  const { state, dispatch } = useApplicationData();
+
+  // const userList = state.users.map((user) => (
+  //   <li key={user.id}>
+  //     {user.id},{user.first_name} ,{user.last_name} ,
+  //     {JSON.parse(user.email).length}
+  //   </li>
+  // ));
+
+  // console.log("Dashboard State", state);
+
+  const globalHistorical = state.globalHistorical;
+
+  let days = [];
+  let cases = [];
+  let casesRecovered = [];
+
+  if (globalHistorical.cases) {
+    // console.log(globalHistorical)
+    const casesObject = globalHistorical.cases;
+    days = Object.keys(casesObject);
+    cases = Object.values(casesObject).map((e) => Number(e) / 1000000);
+
+    const casesRecoveredObject = globalHistorical.recovered;
+    casesRecovered = Object.values(casesRecoveredObject).map(
+      (e) => Number(e) / 1000000
+    );
+  }
+
+  if (state.yesterdayGlobal.updated) {
+  }
+
   const classes = useStyles();
   return (
     <div>
+      {/* <div className='App'>
+        <h1>Users</h1>
+
+        {state.loading && <h3>Loading...</h3>}
+
+        <ul>{!state.loading && userList}</ul>
+      </div>        */}
+
       <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-                <Icon>content_copy</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Used Space</p>
-              <h3 className={classes.cardTitle}>
-                49/50 <small>GB</small>
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={e => e.preventDefault()}>
-                  Get more space
-                </a>
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Store />
-              </CardIcon>
-              <p className={classes.cardCategory}>Revenue</p>
-              <h3 className={classes.cardTitle}>$34,245</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <DateRange />
-                Last 24 Hours
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Fixed Issues</p>
-              <h3 className={classes.cardTitle}>75</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Tracked from Github
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <Accessibility />
-              </CardIcon>
-              <p className={classes.cardCategory}>Followers</p>
-              <h3 className={classes.cardTitle}>+245</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="success">
-              <ChartistGraph
-                className="ct-chart"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Daily Sales</h4>
-              <p className={classes.cardCategory}>
-                <span className={classes.successText}>
-                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
-                </span>{" "}
-                increase in today sales.
-              </p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> updated 4 minutes ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="warning">
-              <ChartistGraph
-                className="ct-chart"
-                data={emailsSubscriptionChart.data}
-                type="Bar"
-                options={emailsSubscriptionChart.options}
-                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
-                listener={emailsSubscriptionChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Email Subscriptions</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="danger">
-              <ChartistGraph
-                className="ct-chart"
-                data={completedTasksChart.data}
-                type="Line"
-                options={completedTasksChart.options}
-                listener={completedTasksChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Completed Tasks</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={6}>
-          <CustomTabs
-            title="Tasks:"
-            headerColor="primary"
-            tabs={[
-              {
-                tabName: "Bugs",
-                tabIcon: BugReport,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0, 3]}
-                    tasksIndexes={[0, 1, 2, 3]}
-                    tasks={bugs}
-                  />
-                )
-              },
-              {
-                tabName: "Website",
-                tabIcon: Code,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0]}
-                    tasksIndexes={[0, 1]}
-                    tasks={website}
-                  />
-                )
-              },
-              {
-                tabName: "Server",
-                tabIcon: Cloud,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[1]}
-                    tasksIndexes={[0, 1, 2]}
-                    tasks={server}
-                  />
-                )
-              }
-            ]}
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType={"Total Recovered"}
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["recoveredPerOneMillion"]
+            }
+            statColor={"success"}
+            statIcon={"accessibility_new_outlines"}
+            updated={state.yesterdayGlobal.updated}
           />
         </GridItem>
-        <GridItem xs={12} sm={12} md={6}>
-          <Card>
-            <CardHeader color="warning">
-              <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
-              <p className={classes.cardCategoryWhite}>
-                New employees on 15th September, 2016
-              </p>
-            </CardHeader>
-            <CardBody>
-              <Table
-                tableHeaderColor="warning"
-                tableHead={["ID", "Name", "Salary", "Country"]}
-                tableData={[
-                  ["1", "Dakota Rice", "$36,738", "Niger"],
-                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
-                ]}
-              />
-            </CardBody>
-          </Card>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType="Cases"
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["casesPerOneMillion"]
+            }
+            statColor={"warning"}
+            statIcon={"local_hospital_outline"}
+            updated={state.yesterdayGlobal.updated}
+          />
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType="Deaths"
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["deathsPerOneMillion"]
+            }
+            statColor={"danger"}
+            statIcon={"warning"}
+            updated={state.yesterdayGlobal.updated}
+          />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType={"Tested"}
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["testsPerOneMillion"]
+            }
+            statColor={"info"}
+            statIcon={"info_outlined"}
+          />
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType={"Active"}
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["activePerOneMillion"]
+            }
+            statColor={"primary"}
+            statIcon={"add_alert_outlined"}
+          />
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <CardDashboard
+            statType={"Critical"}
+            value={
+              state.yesterdayGlobal.updated &&
+              state.yesterdayGlobal["criticalPerOneMillion"]
+            }
+            statColor={"danger"}
+            statIcon={"local_hotel_outlined"}
+          />
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={4}>
+          <CasesChart
+            color="success"
+            title="recovered"
+            days={days}
+            series={casesRecovered}
+            type="Line"
+          />
+        </GridItem>
+
+        <GridItem xs={12} sm={12} md={4}>
+          <ChartPie />
+        </GridItem>
+
+        <GridItem xs={12} sm={12} md={4}>
+          <CasesChart
+            color="danger"
+            title="new"
+            days={days}
+            series={cases}
+            type="Bar"
+            warning="warning"
+          />
         </GridItem>
       </GridContainer>
     </div>
