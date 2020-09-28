c19bf75e6311135505ed7988026858a6d2a44694
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-28 03:52:35 +0000

    Updated icons in the dashboard

--- client/src/views/Icons/Icons.js
+++ client/src/views/Icons/Icons.js
@@ -18,7 +18,49 @@ export default function Icons() {
   const classes = useStyles();
   return (
     <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
+      <GridItem xs={6} sm={6} md={6}>
+        <Card plain>
+          <CardHeader plain color="primary">
+            <h4 className={classes.cardTitleWhite}>Material Design Icons</h4>
+            <p className={classes.cardCategoryWhite}>
+              Handcrafted by our friends from{" "}
+              <a
+                href="https://design.google.com/icons/?ref=creativetime"
+                target="_blank"
+              >
+                Google
+              </a>
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Hidden only={["sm", "xs"]}>
+              <iframe
+                className={classes.iframe}
+                src="https://material.io/icons/"
+                title="Icons iframe"
+              >
+                <p>Your browser does not support iframes.</p>
+              </iframe>
+            </Hidden>
+            <Hidden only={["lg", "md"]}>
+              <GridItem xs={12} sm={12} md={6}>
+                <h5>
+                  The icons are visible on Desktop mode inside an iframe. Since
+                  the iframe is not working on Mobile and Tablets please visit
+                  the icons on their original page on Google. Check the
+                  <a
+                    href="https://design.google.com/icons/?ref=creativetime"
+                    target="_blank"
+                  >
+                    Material Icons
+                  </a>
+                </h5>
+              </GridItem>
+            </Hidden>
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={6} sm={6} md={6}>
         <Card plain>
           <CardHeader plain color="primary">
             <h4 className={classes.cardTitleWhite}>Material Design Icons</h4>
