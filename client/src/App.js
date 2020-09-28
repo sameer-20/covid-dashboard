7dfa7c72575eee1f94b8f094932216f090b1bc45
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-28 03:55:33 +0000

    Added logic to parse user email

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,22 @@ import useApplicationData from './hooks/useApplicationData';
 
 function App() {
   const { state, dispatch } = useApplicationData();
+    
 
   const userList = state.users.map(user => (
-    <li key={user.email}>
-      {user.first_name} {user.last_name} {user.email}
+    <li key={user.id}>
+      {user.id},{user.first_name} ,{user.last_name} ,{JSON.parse(user.email).length}
     </li>
   ));
+const first =state.users[0]
+    if (first) {
+      const data = JSON.parse(first.email)
+      data.find(ele => console.log(ele.country))
+      const oneCountry = data.find(ele => ele.country === 'Albania')
+    console.log(oneCountry.cases)
 
+    }
+    
   return (
     <div className='App'>
       <h1>Users</h1>
